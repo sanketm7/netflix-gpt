f171a55db5cf12ad9f3bf7e7777b5cb9764ba5e3
sanketm7 <sanketmohite6660@gmail.com>
2024-07-26 14:28:46 +0530

    next to do things

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ NeFlix - GPT App
 
 - Create react app
 - install Tiawind
+- install react-router-dom
+- use of Router
+- Login Form
+- Sign Up/ Sign In form
+- form validation
+- useRef Hook
--- src/components/Login.js
+++ src/components/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
   return (
     <div>
       <Header />
-
+      <div>Snaket</div>
       <div className="absolute">
         <img src="https://assets.nflxext.com/ffe/siteui/vlv3/21a8ba09-4a61-44f8-8e2e-70e949c00c6f/6678e2ea-85e8-4db2-b440-c36547313109/IN-en-20240722-POP_SIGNUP_TWO_WEEKS-perspective_WEB_3457a8b1-284d-4bb5-979e-2a2e9bb342b3_small.jpg" />
       </div>
