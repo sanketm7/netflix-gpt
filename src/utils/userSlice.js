1333129136bb546fbe31093fc9241ce5dc1e639a
sanketm7 <sanketmohite6660@gmail.com>
2024-07-29 19:28:16 +0530

    created Store and Slice

---
+++ src/utils/userSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const useSlice = createSlice({
+  name: "user",
+  initialState: null,
+  reducers: {
+    addUser: (state, action) => {
+      return action.payload;
+    },
+    removeUser: (state, action) => {
+      return null;
+    },
+  },
+});
+
+export const { addUser, removeUser } = useSlice.actions;
+
+export default useSlice.reducer;
