bfb260703e2161f9eaf8f1e8f8455538dcc3e55b
sanketm7 <sanketmohite6660@gmail.com>
2024-07-29 20:19:46 +0530

    use of onAuth for signIn and signOut

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
 import Browser from "./Browser";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./Login";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
+
   const appRouter = createBrowserRouter([
     {
       path: "/",
@@ -15,6 +21,17 @@ const Body = () => {
       element: <Browser />,
     },
   ]);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName } = user;
+        dispatch(addUser({ uid: uid, email: email, displayName: displayName }));
+      } else {
+        dispatch(removeUser());
+      }
+    });
+  }, []);
 
   return (
     <div>
